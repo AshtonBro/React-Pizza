517487ce181a90eead58f044ac09e3c9061e062c
Evgenii Solovev <solovyevmeh163@gmail.com>
2020-10-08 23:41:43 +0500

    eject to use

---
+++ src/index.js
@@ -0,0 +1,16 @@
+//? React app
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+//? Import SCSS
+import './index.scss';
+
+//? Import JS
+import App from './App';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root'),
+);
