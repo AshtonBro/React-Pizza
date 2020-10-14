f3b6007739ea571b1b2c4543674906d9197d8776
Evgenii Solovev <solovyevmeh163@gmail.com>
2020-10-14 15:32:48 +0500

    Render <li> from destructuring

---
+++ src/components/index.js
@@ -0,0 +1,10 @@
+// import Categories from './Categories';
+// import Header from './Header';
+// import Button from './Button';
+
+// export { Categories, Header, Button };
+
+//? Import and export in one row
+export { default as Categories } from './Categories';
+export { default as Header } from './Header';
+export { default as Button } from './Button';
